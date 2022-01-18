99be156ca195897cc6d1409119b577d2f7c7cbe7
Chernov Kostiantyn <chernov.konstantin@gmail.com>
2022-01-18 15:27:57 +0200

    Initial Commit

---
+++ GitSearchSwiftUI/GitSearchSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitSearchSwiftUIApp.swift
+//  GitSearchSwiftUI
+//
+//  Created by Константин Чернов on 18.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct GitSearchSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
